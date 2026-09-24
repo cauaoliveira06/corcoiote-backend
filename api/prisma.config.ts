4f1987c57fccaf8a596fb0ab43fa7bc7a1784c4b
Cauã Oliveira <eucaua.oliveira06@gmail.com>
2026-09-24 23:18:47 +0000

    feat: adicionar mapeador objeto-relacional

---
+++ api/prisma.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'prisma/config';
+
+process.loadEnvFile('.env');
+
+
+export default defineConfig({
+  schema: 'prisma/schema.prisma',
+  migrations: {
+    path: 'prisma/migrations'
+  },
+  datasource: {
+    url: process.env.DATABASE_URL
+  }
+});
